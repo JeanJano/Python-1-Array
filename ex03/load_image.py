bdfd60110702976a9137016a34c488451efeca79
JeanJano <jeansauvage6@gmail.com>
2024-06-01 12:03:30 +0200

    finish ex03

--- ex03/load_image.py
+++ ex03/load_image.py
@@ -10,9 +10,8 @@ def ft_load(path: str) -> Array:
         height, width, _ = img.shape
         for y in range(height):
             for x in range(width):
-                if ((y <= 1 and x <= 1) or (y >= height - 1 and x >= width - 1)):
-                    r, g, b = img[y, x]
-                    print(f"[{r}, {g}, {b}]")
+                r, g, b = img[y, x]
+                print(f"[{r}, {g}, {b}]")
 
         return img
 
--- ex03/zoom.py
+++ ex03/zoom.py
@@ -1,10 +1,38 @@
 from load_image import ft_load
+import matplotlib.pyplot as plt
 
-def ft_zoom(path: str) -> None:
-    ft_load(path)
+def ft_zoom(path: str, crop: dict) -> None:
+    try:
+        assert isinstance(crop, dict), "assertion error: crop should be a dictionary"
+        assert all(key in crop for key in ['top', 'bottom', 'left', 'right']), "assertion error: crop should have the keys 'top', 'bottom', 'left', 'right'"
+
+        img = ft_load(path)
+        assert img is not None, "assertion error: image should not be None"
+
+        crop_img = img[crop['top']:crop['bottom'], crop['left']:crop['right']]
+        plt.imshow(crop_img)
+        # plt.show()
+        plt.savefig("crop.jpeg")
+
+        print(f"New size in pixel of the image: x:{crop['right'] - crop['left']} y:{crop['bottom'] - crop['top']}")
+        print(f"New shape after slicing: {crop_img.shape}")
+        height, width, _ = crop_img.shape
+        for y in range(height):
+            for x in range(width):
+                r, g, b = img[y, x]
+                print(f"[{r}, {g}, {b}]")
+    
+    except AssertionError as e:
+        print(e)
 
 def main() -> None:
-    ft_zoom("../animal.jpeg")
+    crop = {
+        'top': 100,
+        'bottom': 500,
+        'left': 400,
+        'right': 900
+    }
+    ft_zoom("../animal.jpeg", crop)
 
 if __name__ == "__main__":
     main()
